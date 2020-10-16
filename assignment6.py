7f364e5df4c7523c0e633282edb1293910f07483
dongree <dongwoo0307@naver.com>
2020-10-16 13:47:36 +0900

    Update assignment6.py

--- assignment6.py
+++ assignment6.py
@@ -1,5 +1,5 @@
 matrix = [[6, 7, 12, 5], [5, 3, 11, 18], [7, 17, 3, 3], [8, 10, 14, 9]]
-distance = [[0]*4]*4
+min_dist = [[0]*4]*4
 
 
 def matrixPath():
@@ -7,32 +7,15 @@ def matrixPath():
     for i in range(n+1):
         for j in range(n+1):
             if (i == 0 and j == 0):
-                distance[i][j] = matrix[0][0]
+                min_dist[i][j] = matrix[0][0]
             elif (i == 0):
-                distance[i][j] = distance[0][j-1] + matrix[i][j]
+                min_dist[i][j] = min_dist[0][j-1] + matrix[i][j]
             elif (j == 0):
-                distance[i][j] = distance[i-1][0] + matrix[i][j]
+                min_dist[i][j] = min_dist[i-1][0] + matrix[i][j]
             else:
-                distance[i][j] = matrix[i][j] + \
-                    min([distance[i-1][j], distance[i][j-1]])
-    return distance[n][n]
+                min_dist[i][j] = matrix[i][j] + \
+                    min([min_dist[i-1][j], min_dist[i][j-1]])
+    return min_dist[n][n]
 
 
 print(matrixPath())
-
-
-# def matrixPath(i, j):
-#     if (distance[i][j] != -1):
-#         return distance[i][j]
-#     if(i == 0 and j == 0):
-#         distance[i][j] = matrix[i][j]
-#     elif (i == 0):
-#         distance[i][j] = matrixPath(i, j-1) + matrix[i][j]
-#     elif (j == 0):
-#         distance[i][j] = matrixPath(i-1, j) + matrix[i][j]
-#     else:
-#         distance[i][j] = min(matrix[i-1][j], matrix[i][j-1]) + matrix[i][j]
-#     return distance[i][j]
-
-
-# print(matrixPath(3, 3))
